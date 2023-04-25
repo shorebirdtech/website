19e50e9ea29107740f83177edfe6ed0675b7b487
Felix Angelov <felix@shorebird.dev>
2023-04-25 17:50:46 -0500

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shorebirdtech.github.io',
+  base: '/website',
   integrations: [react(), tailwind(), mdx()],
 });
