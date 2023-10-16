922c7246ebcf2ae3557b4f6e3c816efd51d9933f
Felix Angelov <felix@shorebird.dev>
2023-10-16 17:01:30 -0500

    tweak

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ export default defineConfig({
     // Some old docs may still point to .html urls for terms and privacy.
     '/terms.html': '/terms',
     '/privacy.html': '/privacy',
+    // Temporarily redirect /pricing to the pricing section on the homepage.
     '/pricing': '/#pricing',
   },
 });
