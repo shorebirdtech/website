d0b61fd50655ae5dc67f85dc5f93775fd1ccffbe
Felix Angelov <felix@shorebird.dev>
2023-10-19 11:56:34 -0500

    add docs

--- src/pages/terms/raw.ts
+++ src/pages/terms/raw.ts
@@ -2,6 +2,9 @@ import * as terms from './index.md';
 
 import type { APIRoute } from 'astro';
 
+// This is an API endpoint that returns a JSON object with the raw markdown content
+// In the future, this can be consolidated with the `terms.md` page however it would
+// require enabling SSR and moving away from the static site generation model.
 export const GET: APIRoute = async () => {
   return new Response(
     JSON.stringify({
