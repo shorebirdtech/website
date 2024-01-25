85f369ca91424e49580e22c9e867f33b2528e394
Eric Seidel <eric@shorebird.dev>
2024-01-25 09:14:10 -0800

    chore: add sitemap
    Google search console suggested we do this?  No clue if it's
    a good idea or worth doing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shorebird.dev',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   redirects: {
     // Some old docs may still point to .html urls for terms and privacy.
     '/terms.html': '/terms',
     '/privacy.html': '/privacy',
-  },
+    '/pricing': '/#pricing'
+  }
 });
