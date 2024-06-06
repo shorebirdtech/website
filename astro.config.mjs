5dbdab755e78d16ff6047da2edcbed1813d7e70a
Eric Seidel <eric@shorebird.dev>
2024-06-06 09:13:50 -0700

    chore: run format (#113)

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,9 @@ export default defineConfig({
     '/terms.html': '/terms',
     '/privacy.html': '/privacy',
     '/pricing': '/#pricing',
+    '/blogs': '/blog',
+    '/blogs/how-we-built-code-push': '/blog/how-we-built-code-push',
+    '/blogs/1.0/': '/blog/1.0',
+    '/blogs/ios-beta/': '/blog/ios-beta',
   },
 });
