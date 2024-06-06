1a45492ff8c541004154e1920deff95161950e91
Felix Angelov <felix@shorebird.dev>
2024-06-06 12:51:55 -0400

    docs: add image to blog and improve blog card styles (#114)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 }
 
 export const Card = (props: CardProps) => (
-  <div className="flex max-w-sm flex-col rounded-xl bg-shorebirdBg3 bg-clip-border text-white shadow-md">
+  <div className="flex max-w-sm flex-col justify-between rounded-xl bg-shorebirdBg3 bg-clip-border text-white shadow-md">
     <div className="p-6">
       <h5 className="text-blue-gray-900 mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal antialiased">
         <a className="hover:underline" href={props.link}>
@@ -18,7 +18,7 @@ export const Card = (props: CardProps) => (
         {props.description}
       </p>
     </div>
-    <div className="p-6 pt-0">
+    <div className="flex flex-col p-6 pt-0">
       <a
         href={props.link}
         className="select-none rounded-lg bg-shorebirdPrimary px-6 py-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:bg-[#0196C0] hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
