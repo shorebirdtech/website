2c49b58da30f0807579490405babfc16751f48a0
Eric Seidel <eric@shorebird.dev>
2024-06-19 13:09:24 -0700

    chore: add redirect for /security

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,5 +17,6 @@ export default defineConfig({
     '/blogs/how-we-built-code-push': '/blog/how-we-built-code-push',
     '/blogs/1.0/': '/blog/1.0',
     '/blogs/ios-beta/': '/blog/ios-beta',
+    '/security': 'https://handbook.shorebird.dev/security',
   },
 });
