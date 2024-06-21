c24080498edd76cd11010f5a8ba951d8ebcaeaad
Eric Seidel <eric@shorebird.dev>
2024-06-21 12:21:25 -0700

    chore: remove 1.0 blog announce banner
    1.0 was a while ago :)

--- src/components/StableBanner.tsx
+++
@@ -1,8 +0,0 @@
-export const StableBanner = () => (
-  <div className="absolute mt-20 w-full animate-gradient-xy bg-gradient-to-r from-blue-400 via-purple-500 to-teal-500 p-2 text-center text-lg font-semibold text-white">
-    🎉{' '}
-    <a href="/blog/1.0" className="underline">
-      Announcing Shorebird 1.0! →
-    </a>
-  </div>
-);
