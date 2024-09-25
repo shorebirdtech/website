771e4b76e3a54ff9963972cb67888b558b39ff58
Eric Seidel <eric@shorebird.dev>
2024-09-25 13:50:05 -0700

    feat: add jobs banner

---
+++ src/components/JobsBanner.tsx
@@ -0,0 +1,8 @@
+export const JobsBanner = () => (
+    <div className="absolute mt-20 w-full animate-gradient-xy bg-gradient-to-r from-blue-400 via-purple-500 to-teal-500 p-2 text-center text-lg font-semibold text-white">
+      🎉{' '}
+      <a href="/jobs" className="underline">
+        We're hiring! →
+      </a>
+    </div>
+  );
