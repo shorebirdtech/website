fdb04cbec1e305bb3e0925ba1f11765694e9ea1d
Eric Seidel <eric@shorebird.dev>
2024-09-26 09:37:15 -0700

    chore: run npm format

--- src/components/PricingDetails.tsx
+++ src/components/PricingDetails.tsx
@@ -14,21 +14,62 @@ interface Feature {
 }
 
 const features: Feature[] = [
-    { title: 'Unlimited Apps, Releases and Patches', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
-    { title: 'Console', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
-    { title: 'Rollbacks', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
-    { title: 'Notifications', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
-    { title: 'Staging', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
-    { title: 'Storage', free: '1 Month', pro: '1 Year', teams: 'Unlimited', enterprise: 'Unlimited' },
-    { title: 'Analytics', free: 'Basic', pro: 'Basic', teams: 'Advanced', enterprise: 'Advanced' },
-    { title: 'Support', free: 'Community', pro: 'Semi-Private', teams: 'Private', enterprise: 'Personal' },
-    { title: 'Assets', free: '1mb', pro: '10mb', teams: 'Unlimited', enterprise: 'Unlimited' },
-    { title: 'Organizations', pro: 'Basic', teams: 'Advanced', enterprise: 'Custom' },
-    { title: 'User Roles', free: 'Admin, Developer', pro: 'Admin, Developer', teams: 'Admin, Developer, Billing, View-Only', enterprise: 'Custom' },
-    { title: 'Annual Billing', teams: '✓', enterprise: '✓' },
-    { title: 'White-Label Support', teams: '✓', enterprise: '✓' },
-    { title: 'Signed Patches', pro: '✓', teams: '✓', enterprise: '✓' },
-    { title: 'SAML', enterprise: 'Custom' },
+  {
+    title: 'Unlimited Apps, Releases and Patches',
+    free: '✓',
+    pro: '✓',
+    teams: '✓',
+    enterprise: '✓',
+  },
+  { title: 'Console', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
+  { title: 'Rollbacks', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
+  { title: 'Notifications', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
+  { title: 'Staging', free: '✓', pro: '✓', teams: '✓', enterprise: '✓' },
+  {
+    title: 'Storage',
+    free: '1 Month',
+    pro: '1 Year',
+    teams: 'Unlimited',
+    enterprise: 'Unlimited',
+  },
+  {
+    title: 'Analytics',
+    free: 'Basic',
+    pro: 'Basic',
+    teams: 'Advanced',
+    enterprise: 'Advanced',
+  },
+  {
+    title: 'Support',
+    free: 'Community',
+    pro: 'Semi-Private',
+    teams: 'Private',
+    enterprise: 'Personal',
+  },
+  {
+    title: 'Assets',
+    free: '1mb',
+    pro: '10mb',
+    teams: 'Unlimited',
+    enterprise: 'Unlimited',
+  },
+  {
+    title: 'Organizations',
+    pro: 'Basic',
+    teams: 'Advanced',
+    enterprise: 'Custom',
+  },
+  {
+    title: 'User Roles',
+    free: 'Admin, Developer',
+    pro: 'Admin, Developer',
+    teams: 'Admin, Developer, Billing, View-Only',
+    enterprise: 'Custom',
+  },
+  { title: 'Annual Billing', teams: '✓', enterprise: '✓' },
+  { title: 'White-Label Support', teams: '✓', enterprise: '✓' },
+  { title: 'Signed Patches', pro: '✓', teams: '✓', enterprise: '✓' },
+  { title: 'SAML', enterprise: 'Custom' },
 ];
 
 export const PricingDetails = () => {
@@ -37,20 +78,20 @@ export const PricingDetails = () => {
       <div className="absolute -top-16" id="details" />
       <div className="container mx-auto px-4">
         <table>
-            <thead>
-              <tr>
-                <th className="text-left">Feature</th>
-                <th className="text-left">Free</th>
-                <th className="text-left">Pro</th>
-                <th className="text-left">Teams</th>
-                <th className="text-left">Enterprise</th>
-              </tr>
-            </thead>
-            <tbody>
-                {features.map((feature) => (
-                    <FeatureRow key={feature.title} {...feature} />
-                ))}
-            </tbody>
+          <thead>
+            <tr>
+              <th className="text-left">Feature</th>
+              <th className="text-left">Free</th>
+              <th className="text-left">Pro</th>
+              <th className="text-left">Teams</th>
+              <th className="text-left">Enterprise</th>
+            </tr>
+          </thead>
+          <tbody>
+            {features.map((feature) => (
+              <FeatureRow key={feature.title} {...feature} />
+            ))}
+          </tbody>
         </table>
       </div>
     </section>
@@ -58,14 +99,13 @@ export const PricingDetails = () => {
 };
 
 function FeatureRow(feature: Feature) {
-    return (
-      <tr>
-        <td>{feature.title}</td>
-        <td>{feature.free}</td>
-        <td>{feature.pro}</td>
-        <td>{feature.teams}</td>
-        <td>{feature.enterprise}</td>
-      </tr>
-    );
-  }
-  
+  return (
+    <tr>
+      <td>{feature.title}</td>
+      <td>{feature.free}</td>
+      <td>{feature.pro}</td>
+      <td>{feature.teams}</td>
+      <td>{feature.enterprise}</td>
+    </tr>
+  );
+}
