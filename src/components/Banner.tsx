c66ec3205d8c5addaad7b0f630407329c54dabc1
Felix Angelov <felix@shorebird.dev>
2025-01-06 16:23:06 -0600

    feat: update banner to link to workshop (#223)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,8 @@
-export const JobsBanner = () => (
+export const Banner = () => (
   <div className="absolute z-50 mt-20 w-full animate-gradient-xy bg-gradient-to-r from-blue-400 via-purple-500 to-teal-500 p-2 text-center text-lg font-semibold text-white">
-    🎉{' '}
-    <a href="/jobs" className="underline">
-      We're hiring! →
-    </a>
+    <a href="/blog/workshops" className="hover:underline">
+      Enroll in a free online workshop
+    </a>{' '}
+    🎓
   </div>
 );
