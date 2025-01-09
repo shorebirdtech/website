6d8e5f1fda9a58e1cb6f41f537d7afc7ac302de9
Felix Angelov <felix@shorebird.dev>
2025-01-09 13:37:17 -0600

    adjust endpoints

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,10 @@ export const config = {
   githubUrl: 'https://github.com/shorebirdtech/shorebird',
   twitterUrl: 'https://twitter.com/shorebirddev',
   consoleUrl: 'https://console.shorebird.dev',
-  proPlanCheckoutUrl: 'https://console.shorebird.dev/subscriptions/pro/create',
+  proPlanCheckoutUrl:
+    'https://console.shorebird.dev/subscriptions/create?plan=pro',
   businessPlanCheckoutUrl:
-    'https://console.shorebird.dev/subscriptions/business/create',
+    'https://console.shorebird.dev/subscriptions/create?plan=business',
   contactSales: 'https://calendly.com/eseidel/shorebird-sales',
   contactEmail: 'contact@shorebird.dev',
   navLinks: [
