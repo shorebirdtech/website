c9e54a4a066aaa1acaba6f3ff458deaa55757e9d
Bryan Oltman <bryanoltman@gmail.com>
2025-02-07 13:11:45 -0500

    feat: add desktop production blog post announcement (#246)
    * feat: add desktop production blog post announcement
    
    * update banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 export const Banner = () => (
   <div className="absolute z-50 mt-20 w-full animate-gradient-xy bg-gradient-to-r from-blue-400 via-purple-500 to-teal-500 p-2 text-center text-lg font-semibold text-white">
-    <a href="/blog/windows-beta" className="hover:underline">
-      Windows support is now in beta! Learn more
+    <a href="/blog/desktop-in-production" className="hover:underline">
+      Desktop support is now stable!
     </a>{' '}
     🎉
   </div>
