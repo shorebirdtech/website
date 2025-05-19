79452f230fdbb4e2861a39954bdafa0a5f78b868
Felix Angelov <felix@shorebird.dev>
2025-05-19 17:26:25 -0500

    chore: update star count and number of users

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -47,7 +47,7 @@ function Navbar() {
             <a className="hidden lg:block" href={config.githubUrl}>
               <Button variant="outline" size="sm" className="font-light">
                 <GitHubLogo className="size-5" />
-                2.5k
+                2.6k
               </Button>
             </a>
             <a href={config.consoleUrl}>
